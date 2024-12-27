b9c4d85e03cc0b790129e4da06ce51910e37d8cf
Anatoly <anatoliyabrashkin1@gmail.com>
2024-12-27 13:19:32 +0300

    first

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "std.h"
+#include <iostream>
+using namespace std;
+int main()
+{  // comment
+    int a, b;
+    std::cout << "Hello";
+    std::cout << " World\n";
+    std::cin >>a; std::cout << std::endl; 
+    std::cin >>b;std::cout << std::endl;
+    cout << a +b;
+    cout << a *b << "MULTIPLICATION";
+    cout << a /b << "DIVISION";
+}
